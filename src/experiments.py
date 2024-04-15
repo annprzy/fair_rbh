6743625b04f6282be31ea54a743fb7c28464cca9
annprzy <anneprzybylowska@gmail.com>
2024-04-15 17:10:45 +0200

    update experiments

--- src/experiments.py
+++ src/experiments.py
@@ -1,4 +1,8 @@
 import os
+
+import sys
+sys.path.insert(0, 'src')
+
 from itertools import product
 
 import numpy as np
@@ -9,6 +13,7 @@ from neptune.utils import stringify_unsupported
 from sklearn.model_selection import KFold
 
 from src.classification.logistic_regression import LogisticRegressor
+from src.classification.decision_tree import DecisionTree
 from src.datasets.adult_dataset import AdultDataset
 from src.datasets.bank_dataset import BankDataset
 from src.datasets.credit_card_dataset import CreditCardDataset
@@ -20,28 +25,31 @@ from src.preprocessing.FOS import oversamplor as FOS
 from src.preprocessing.FOS_original import oversamplor as FOS_org
 from src.preprocessing.HFOS import oversamplor as HFOS
 
+import warnings
+warnings.filterwarnings("ignore")
+
 
 def init_dataset(dataset_name, random_state):
     if dataset_name == 'german':
-        return GermanDataset('../data/german_credit/german.data', binary=True, group_type='', random_state=random_state)
+        return GermanDataset('data/german_credit/german.data', binary=True, group_type='', random_state=random_state)
     elif dataset_name == 'bank':
-        return BankDataset('../data/bank_marketing/bank-full.csv', binary=True, group_type='',
+        return BankDataset('data/bank_marketing/bank-full.csv', binary=True, group_type='',
                            random_state=random_state)
     elif dataset_name == 'adult':
-        return AdultDataset('../data/adult_census/adult.data', binary=True, group_type='', random_state=random_state)
+        return AdultDataset('data/adult_census/adult.data', binary=True, group_type='', random_state=random_state)
 
 
 def run_oversampling(algorithm: str, dataset: Dataset):
     if algorithm == 'fos':
-        with open('../configs/preprocessing/fos.yml') as f:
+        with open('configs/preprocessing/fos.yml') as f:
             fos_cfg = yaml.safe_load(f)
         FOS.run(dataset, k=fos_cfg['k'], oversampling_factor=fos_cfg['oversampling_factor'])
     if algorithm == 'hfos':
-        with open('../configs/preprocessing/hfos.yml') as f:
+        with open('configs/preprocessing/hfos.yml') as f:
             hfos_cfg = yaml.safe_load(f)
         HFOS.run(dataset, k=hfos_cfg['k'])
     if algorithm == 'fawos':
-        with open('../configs/preprocessing/fawos.yml') as f:
+        with open('configs/preprocessing/fawos.yml') as f:
             fawos_cfg = yaml.safe_load(f)
         FAWOS.run(dataset, fawos_cfg['safe_weight'], fawos_cfg['borderline_weight'],
                   fawos_cfg['rare_weight'], oversampling_factor=fawos_cfg['oversampling_factor'])
@@ -50,7 +58,9 @@ def run_oversampling(algorithm: str, dataset: Dataset):
 
 def get_model(model_name: str):
     if model_name == 'logistic_regression':
-        return LogisticRegressor(cfg_path='../configs/classifiers/logistic_regression.yml')
+        return LogisticRegressor(cfg_path='configs/classifiers/logistic_regression.yml')
+    elif model_name == 'decision_tree':
+        return DecisionTree(cfg_path='configs/classifiers/decision_tree.yml')
 
 
 def experiment(dataset_name: str, algorithm: str, model_name: str, iteration: int, date: str, random_seed: int,
@@ -58,7 +68,7 @@ def experiment(dataset_name: str, algorithm: str, model_name: str, iteration: in
     dataset = init_dataset(dataset_name, random_seed)
     if kfolds is not None:
         kf = KFold(n_splits=kfolds, shuffle=True, random_state=42)
-        dataset_train = dataset.train
+        dataset_train = dataset.data
         results = list(kf.split(dataset_train))[iteration]
         train_set, test_set = results
         dataset.train = dataset_train.iloc[train_set].reset_index(drop=True)
@@ -67,20 +77,27 @@ def experiment(dataset_name: str, algorithm: str, model_name: str, iteration: in
     dataset = run_oversampling(algorithm, dataset)
     print(f'{algorithm}, {dataset_name}, {model_name}, {iteration} \nPrivileged: {dataset.privileged_groups} \n{dataset.train.shape}, {dataset.fair.shape} \nStats train: {dataset.get_stats_data(dataset.train)}Stats fair: {dataset.get_stats_data(dataset.fair)}')
 
+    if not os.path.exists(f'results/{algorithm}_{dataset_name}_{model_name}/'):
+        os.makedirs(f'results/{algorithm}_{dataset_name}_{model_name}/')
+    if not os.path.exists(f'results/{algorithm}_{dataset_name}_{model_name}/{date}'):
+        os.makedirs(f'results/{algorithm}_{dataset_name}_{model_name}/{date}')
+
+    dataset.train.to_csv(f'results/{algorithm}_{dataset_name}_{model_name}/{date}/train_{iteration}.csv')
+    dataset.fair.to_csv(f'results/{algorithm}_{dataset_name}_{model_name}/{date}/fair_{iteration}.csv')
+
     model = get_model(model_name)
     model.load_model()
+
     model.train(dataset=dataset, data='train', enc_type=enc_type)
     perf_train_small, fairness_train_small = model.predict_and_evaluate(dataset=dataset,
                                                                         fairness_type='binary',
                                                                         enc_type=enc_type)
 
-    model = get_model(model_name)
-    model.load_model()
     model.train(dataset=dataset, data='fair', enc_type=enc_type)
     perf_fair_small, fairness_fair_small = model.predict_and_evaluate(dataset=dataset,
-                                                                      fairness_type='binary',
-                                                                      enc_type=enc_type)
-
+                                                                    fairness_type='binary',
+                                                                    enc_type=enc_type)
+    
     perf_train_small['data'] = f'train_{enc_type}'
     fairness_train_small['data'] = f'train_{enc_type}'
     perf_fair_small['data'] = f'fair_{enc_type}'
@@ -88,41 +105,43 @@ def experiment(dataset_name: str, algorithm: str, model_name: str, iteration: in
     perf = pd.concat([pd.DataFrame(perf_train_small, index=[iteration]), pd.DataFrame(perf_fair_small, index=[iteration])])
     fairness = pd.concat([pd.DataFrame(fairness_train_small, index=[iteration]), pd.DataFrame(fairness_fair_small, index=[iteration])])
 
-    if not os.path.exists(f'../results/{algorithm}_{dataset_name}_{model_name}/'):
-        os.makedirs(f'../results/{algorithm}_{dataset_name}_{model_name}/')
-    if not os.path.exists(f'../results/{algorithm}_{dataset_name}_{model_name}/{date}'):
-        os.makedirs(f'../results/{algorithm}_{dataset_name}_{model_name}/{date}')
-    perf.to_csv(f'../results/{algorithm}_{dataset_name}_{model_name}/performance_{iteration}.csv',
+    perf.to_csv(f'results/{algorithm}_{dataset_name}_{model_name}/{date}/performance_{iteration}.csv',
                 index=False)
-    fairness.to_csv(f'../results/{algorithm}_{dataset_name}_{model_name}/fairness_{iteration}.csv',
+    fairness.to_csv(f'results/{algorithm}_{dataset_name}_{model_name}/{date}/fairness_{iteration}.csv',
                     index=False)
+        
+
+    
 
 
 if __name__ == "__main__":
-    datasets = ['german'] #, 'adult', 'bank']
-    algorithms = ['hfos'] #, 'fos', 'fawos']
-    models = ['logistic_regression']
-    kfolds = 2
-    encoding = 'cont_ord_cat'
-    date = '2023-03-28'
+    datasets = ['adult', 'bank']
+    algorithms = ['hfos', 'fos']
+    models = ['logistic_regression', 'decision_tree']
+    kfolds = 6
+    encoding = 'cont'
+    date = '2023-03-28-cont'
     iterations = [i for i in range(0, kfolds)]
     seeds = [42+i for i in iterations]
-    all_options = list(product(datasets, algorithms, models, iterations, seeds))
+    all_options = list(product(datasets, algorithms, models, iterations))
     neptune = True
-    with open('../configs/neptune.yml') as f:
-        cfg = yaml.safe_load(f)
 
     neptune_run = None
     if neptune:
+        with open('configs/neptune.yml') as f:
+            cfg = yaml.safe_load(f)
         neptune_run = init_neptune(cfg)
         log_results(neptune_run, {'algorithm': str(algorithms), 'datasets': str(datasets), 'encoding': encoding, 'folds': kfolds}, 'basic_info')
 
-    Parallel(n_jobs=-1)(delayed(experiment)(d_name, a, m_name, idx, date, rnd_seed, kfolds=kfolds, enc_type=encoding) for d_name, a, m_name, idx, rnd_seed in all_options)
+    Parallel(n_jobs=-1)(delayed(experiment)(d_name, a, m_name, idx, date, seeds[idx], kfolds=kfolds, enc_type=encoding) for d_name, a, m_name, idx in all_options)
 
     if neptune:
-        for d_name, a, m_name, idx, rnd_seed in all_options:
-            perf = pd.read_csv(f'../results/{a}_{d_name}_{m_name}/performance_{idx}.csv')
-            fairness = pd.read_csv(f'../results/{a}_{d_name}_{m_name}/fairness_{idx}.csv')
-            log_results(neptune_run, fairness, f'{a}_{d_name}_{m_name}/fairness_{idx}')
-            log_results(neptune_run, perf, f'{a}_{d_name}_{m_name}/performance_{idx}')
+        for d_name, a, m_name, idx in all_options:
+            try:
+                perf = pd.read_csv(f'results/{a}_{d_name}_{m_name}/{date}/performance_{idx}.csv')
+                fairness = pd.read_csv(f'results/{a}_{d_name}_{m_name}/{date}/fairness_{idx}.csv')
+                log_results(neptune_run, fairness, f'{a}_{d_name}_{m_name}/{date}/fairness_{idx}')
+                log_results(neptune_run, perf, f'{a}_{d_name}_{m_name}/{date}/performance_{idx}')
+            except:
+                pass
         neptune_run.stop()
